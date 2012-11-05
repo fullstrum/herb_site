945e50658cb54484013a3b5b53f2f89b24508fe6
Chad Blank <cb1301@gmail.com>
2012-11-04 20:13:57 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HerbSite::Application.config.secret_token = '7cc46a39d25f9123ebf376cd7d60b9a6c04eff2071467eb053764b2077fe8e656ce4c64ddb861bbf1e3415cd8cce56ab92045cc7f38a12be12db1a77c6cea92a'
