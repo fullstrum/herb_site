1928dd6074018cbc7ed3382e020654b0e28c9a8e
Chad Blank <cb1301@gmail.com>
2012-11-25 01:51:53 -0500

    finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ActiveRecord::Base
 
 
 	before_save { |user| user.email = email.downcase }
-	
+	before_save :create_remember_token
+
 	validates :firstName, presence: true, length: { maximum: 50}
 	validates :lastName, presence: true, length: { maximum: 50}
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -12,5 +13,11 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
     validates :password, length: { minimum: 6 }
     validates :password_confirmation, presence: true
+    
+    private
+
+	    def create_remember_token
+	      self.remember_token = SecureRandom.urlsafe_base64
+	    end
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 HerbSite::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   root to: 'static_pages#home'
 
   match '/signup', to: 'users#new'
-
-  match '/contact', to:"static_pages#contact"
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
+  match '/contact', to: "static_pages#contact"
 
   
 
